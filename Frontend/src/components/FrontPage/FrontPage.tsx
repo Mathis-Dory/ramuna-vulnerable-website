e2edb1037d6abb7be29107e8d2aeedea2f4d6354
mathisdory <mathisdory@gmail.com>
2023-03-12 16:13:31 +0100

    feat(frontpage): Add first page
    * Add empty first page

---
+++ Frontend/src/components/FrontPage/FrontPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface FrontPageProps {}
+
+const FrontPage: FC<FrontPageProps> = () => (
+  <div>
+    FrontPage Component
+  </div>
+);
+
+export default FrontPage;
