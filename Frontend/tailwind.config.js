97841939a326e2fe3bd75bc7ed70268a54e48f60
mathisdory <mathisdory@gmail.com>
2023-03-12 17:17:53 +0100

    style(navbar): Change style of navigation bar
    * Use new theme

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,4 +5,29 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        romunia: {
+          primary: "#293462",
+
+          secondary: "#D61C4E",
+
+          accent: "#FEB139",
+
+          black: "#000000",
+
+          white: "#FFFFFF",
+
+          info: "rgba(213,51,189,0.6)",
+
+          success: "rgba(21,176,52,0.78)",
+
+          warning: "#FBBD23",
+
+          error: "rgba(255,0,0,0.73)",
+        },
+      },
+    ],
+  },
 };
