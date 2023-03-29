c5c85b841b93ce4b73ff68d2efccf8aad44ed2f9
mathisdory <mathisdory@gmail.com>
2023-03-29 14:12:35 +0300

    chore(backend): apply prettier
    * reformat file

--- Backend/src/users/users.module.ts
+++ Backend/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../typeorm/user.entity';
+import { User } from '../typeorm';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
 
