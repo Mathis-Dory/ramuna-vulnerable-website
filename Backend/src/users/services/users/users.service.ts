a796cebdb42c66c728a8f044cbd06d6631fdfffe
mathisdory <mathisdory@gmail.com>
2023-03-29 14:12:35 +0300

    chore(backend): apply prettier
    * reformat file

--- Backend/src/users/services/users/users.service.ts
+++ Backend/src/users/services/users/users.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm';
-import { LoginUserDto, RegisterUserDto } from '../../../users/dto/users.dtos';
+import { LoginUserDto, RegisterUserDto } from '../../dto/users.dtos';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
