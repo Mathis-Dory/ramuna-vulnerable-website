c7db65f2803e3679422b90dc157ad059affa958c
mathisdory <mathisdory@gmail.com>
2023-04-04 16:55:02 +0300

    chore(backend): change port
    * Use .env PORT for the port of the backend
    * Add starting message

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`========== Nest server successfully started on port ${port} ==========`);
 }
 bootstrap();
