0366d92617b7263c228ced082ea02b77e321b9aa
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2023-04-24 14:23:07 +0200

    Delete Frontend/src/components/RegistrationPage directory

--- Frontend/src/components/RegistrationPage/AdminRegistrationPage.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-interface AdminRegistrationPageProps {}
-
-const AdminRegistrationPage: FC<AdminRegistrationPageProps> = () => {
-  return <div></div>;
-};
-
-export default AdminRegistrationPage;
