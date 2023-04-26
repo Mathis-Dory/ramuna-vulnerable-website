df4f1da4ca471b265f63056091cbbb0b82c9d34b
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2023-04-26 14:52:40 +0300

    Update Axios.tsx

--- cybersecurity_ctf/Frontend/src/shared/utils/Axios.tsx
+++ cybersecurity_ctf/Frontend/src/shared/utils/Axios.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-const baseURL = "http://localhost:3003/";
+const baseURL = "http://localhost:8000/";
 
 export const axiosInstance = axios.create({
   baseURL,
